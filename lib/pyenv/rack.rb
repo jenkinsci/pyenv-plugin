7fd3f5fa1a520ef6ef828db9db6ca7a4e6a03513
Yamashita Yuu <yamashita@geishatokyo.com>
2013-06-06 19:20:40 +0900

    add validation for PYENV_ROOT

--- lib/pyenv/rack.rb
+++ lib/pyenv/rack.rb
@@ -18,5 +18,14 @@ module Pyenv
         response.body = Java.hudson.util.FormValidation.ok().renderHtml
       end
     end
+
+    get "/checkPyenvRoot" do
+      value = params[:value]
+      if value.nil? or value.to_s.strip.empty?
+        response.body = Java.hudson.util.FormValidation.error("The PYENV_ROOT must not be empty.").renderHtml
+      else
+        response.body = Java.hudson.util.FormValidation.ok().renderHtml
+      end
+    end
   end
 end

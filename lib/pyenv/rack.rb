3fef7eba0be8d64f8e8e04b51438f12f36fb42e1
Miglen Evlogiev <github@miglen.com>
2021-06-17 02:07:35 +0300

    Add suppport for POST methods

--- lib/pyenv/rack.rb
+++ lib/pyenv/rack.rb
@@ -5,27 +5,35 @@ require "sinatra"
 
 module Pyenv
   class RackApplication < Sinatra::Base
+
+    def check_value(value, parameter)
+      if value.nil? or value.to_s.strip.empty?
+        Java.hudson.util.FormValidation.error("The #{parameter} must not be empty.").renderHtml
+      else
+        Java.hudson.util.FormValidation.ok().renderHtml
+      end
+    end
+
     # GET /descriptorByName/pyenv-PyenvWrapper/ping
     get "/ping" do
       "pong"
     end
 
     get "/checkVersion" do
-      value = params[:value]
-      if value.nil? or value.to_s.strip.empty?
-        response.body = Java.hudson.util.FormValidation.error("The version string must not be empty.").renderHtml
-      else
-        response.body = Java.hudson.util.FormValidation.ok().renderHtml
-      end
+      response.body = check_value(params[:value], 'version string')
     end
 
     get "/checkPyenvRoot" do
-      value = params[:value]
-      if value.nil? or value.to_s.strip.empty?
-        response.body = Java.hudson.util.FormValidation.error("The PYENV_ROOT must not be empty.").renderHtml
-      else
-        response.body = Java.hudson.util.FormValidation.ok().renderHtml
-      end
+      response.body = check_value(params[:value], 'PYENV_ROOT')
     end
+
+    post "/checkVersion" do
+      response.body = check_value(params[:value], 'version string')
+    end
+
+    post "/checkPyenvRoot" do
+      response.body = check_value(params[:value], 'PYENV_ROOT')
+    end
+
   end
 end
